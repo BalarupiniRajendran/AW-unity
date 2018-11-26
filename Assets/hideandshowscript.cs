53eee116358ffa32fab91b330fd7076632a9b1ca
Balarupini Rajendran <balarupini@gmail.com>
2018-11-25 18:42:04 -0700

    adding task for the user

---
+++ Assets/hideandshowscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class hideandshowscript : MonoBehaviour {
+
+	public GameObject Panel;
+	int counter;
+
+
+	void Start () {
+		Panel = GameObject.Find("helpPanel");
+		Panel.SetActive (false);
+	}
+
+	public void showHidePanel(){
+		
+//		counter++;
+//		if (counter % 2 == 0) {
+//			Panel.gameObject.SetActive (false);
+//		}
+//		else{
+//			Panel.gameObject.SetActive (true);
+//		}
+
+		Panel.SetActive (!Panel.activeSelf);
+	}
+		
+}
---
+++ Assets/showandhidequestionscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showandhidequestionscript : MonoBehaviour {
+
+	public GameObject Qpanel;
+	int counter;
+
+
+	// Use this for initialization
+	void Start () {
+		Qpanel = GameObject.Find("questionPanel");
+		Qpanel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showHideqPanel (){
+		Qpanel.SetActive (!Qpanel.activeSelf);
+	}
+
+}
